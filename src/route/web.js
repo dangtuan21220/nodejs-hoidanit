4bd33613be3a1bcfb541cf0ac5ae8d8ae592c619
tuandv21 <tuandv@edsolabs.com>
2022-09-21 17:50:23 +0700

    CRUD 21/09/2022

--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,12 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomepage);
+  router.get("/detail/user/:id", homeController.getDetailPage);
+  router.post("/create-new-user", homeController.createNewUser);
+  router.post("/delete-user", homeController.deleteUser);
+  router.get("/edit-user/:id", homeController.editUser);
+  router.post("/update-user", homeController.updateUser);
+
   router.get("/about", (req, res) => {
     res.send("I'm Tuấnaa");
   });
